6d9b5672a4d27adaeb7a20c98555b0e6e6f58b75
chetan tyagi <chaitanyatyagi1540@gmail.com>
2023-09-18 22:46:28 +0530

    message same side after reload bug left

---
+++ src/utils/APIRoutes.js
@@ -0,0 +1,9 @@
+export const host = "http://localhost:5011"
+export const registerRoute = `${host}/api/auth/register`
+export const loginRoute = `${host}/api/auth/login`
+export const logoutRoute = `${host}/api/auth/logout`;
+export const setAvatarRoute = `${host}/api/auth/setAvatar`
+export const setAvatarImageRoute = `${host}/api/auth/setAvatar/images`
+export const allUsersRoute = `${host}/api/auth/allusers`
+export const sendMessageRoute = `${host}/api/messages/addmsg`;
+export const recieveMessageRoute = `${host}/api/messages/getmsg`;
